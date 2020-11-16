ea85dd5a6911f2e92e3b90efc57b62c66facb3be
Glen D Fabert <glen.fabert@gmail.com>
2020-11-15 20:21:59 -0500

    added components file

---
+++ src/components/EmployeeCard/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Role:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Department:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeEmployee(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default EmployeeCard;
